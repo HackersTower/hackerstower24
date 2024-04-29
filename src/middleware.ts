2a4535a5586553dde0a0fc5f4fb3d1ead6880632
Sumit pal <tonystark83033@gmail.com>
2024-04-30 02:51:30 +0530

    fix:[allow access of /workshop/:path*]

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({});
-
+export default authMiddleware({
+  publicRoutes: ["/", "/workshop", "/workshop/:slug"],
+});
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
